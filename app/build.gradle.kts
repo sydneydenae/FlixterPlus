bfa4a3a841d104d2b8b457f521b1ab539f494494
sydneydenae <sydneydenae1@gmail.com>
2024-09-26 13:22:25 -0400

    Added glide dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    // Glide dependencies
+    implementation(libs.glide)
+    annotationProcessor(libs.glide.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
